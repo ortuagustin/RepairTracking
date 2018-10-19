eeaf25cb79dd6921b0289e179297885ab8e5a662
Agustin ORTU <ortu.agustin@gmail.com>
2018-10-18 21:02:53 -0300

    por el momento el check de autenticacion en todas las paginas

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :set_locale
-  before_action :authenticate_user!, unless: :devise_controller?
 protected
   def set_locale
     I18n.locale = locale_params[:locale] || session[:locale] || I18n.default_locale
