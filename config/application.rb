424978583386993d445cdc9714a9cf5e73b2fdd9
Agustin ORTU <ortu.agustin@gmail.com>
2018-10-18 21:02:21 -0300

    idioma por defecto español; agrego /lib al autoload

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,8 @@ module RepairTracking
     config.load_defaults 5.2
 
     config.i18n.available_locales = [:en, :es, :langs]
+    config.i18n.default_locale = :es
+    config.autoload_paths += %W(#{config.root}/lib)
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
