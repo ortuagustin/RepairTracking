59f3de32d1c7c7025a41e1c776e2d4b316e341d0
Agustin ORTU <ortu.agustin@gmail.com>
2018-10-18 23:38:51 -0300

    elimino "rememberable" del usuario

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,6 @@ ActiveRecord::Schema.define(version: 2018_10_18_164058) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
