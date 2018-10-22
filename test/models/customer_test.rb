ff3a06abf2118723540f569103bf08363836b2e0
Agustin ORTU <ortu.agustin@gmail.com>
2018-10-22 13:46:07 -0300

    agrego tests para los Clientes

--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -1,7 +1,56 @@
 require 'test_helper'
 
 class CustomerTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @agus = customers(:agus)
+  end
+
+  test "should accept customer with valid attributes" do
+    assert @agus.valid?
+  end
+
+  test "should not accept customer without a name" do
+    @agus.name = nil
+    assert @agus.invalid?
+  end
+
+  test "should not accept customer with a blank name" do
+    @agus.name = ''
+    assert @agus.invalid?
+  end
+
+  test "should not accept customer without a surname" do
+    @agus.surname = nil
+    assert @agus.invalid?
+  end
+
+  test "should not accept customer with a blank surname" do
+    @agus.surname = ''
+    assert @agus.invalid?
+  end
+
+  test "should not accept customer without a email" do
+    @agus.email = nil
+    assert @agus.invalid?
+  end
+
+  test "should not accept customer with a blank email" do
+    @agus.email = ''
+    assert @agus.invalid?
+  end
+
+  test "should not accept customer without a phone" do
+    @agus.phone = nil
+    assert @agus.invalid?
+  end
+
+  test "should not accept customer with a blank phone" do
+    @agus.phone = ''
+    assert @agus.invalid?
+  end
+
+  test "should not accept customer with a non numeric phone" do
+    @agus.phone = 'test'
+    assert @agus.invalid?
+  end
 end
