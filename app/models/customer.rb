0cee2484424a4e5e1f57a10659c80f3e9fe6b295
Agustin ORTU <ortu.agustin@gmail.com>
2018-10-22 21:32:45 -0300

    aumento el tamaño de los campos tipo string en los Clientes

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,6 +1,6 @@
 class Customer < ApplicationRecord
   validates :name, :surname, :email, :phone, presence: true
-  validates :name, :surname, :email, length: { maximum: 30 }
+  validates :name, :surname, :email, length: { maximum: 50 }
   validates :phone, format: { with: /(\d{3}-)?\d{3}-\d{4}/, message: :invalid_phone }
   validates :email, email: { message: :invalid_email }
 end
