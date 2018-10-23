d9c47fd42666bb71e0bb702e382e3f95103eee4a
Agustin ORTU <ortu.agustin@gmail.com>
2018-10-23 16:44:29 -0300

    Asociacion entre Cliente y sus Reparaciones

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,6 @@
 class Customer < ApplicationRecord
+  has_many :repairs
+
   validates :name, :surname, :email, :phone, presence: true
   validates :name, :surname, :email, length: { maximum: 50 }
   validates :phone, format: { with: /(\d{3}-)?\d{3}-\d{4}/, message: :invalid_phone }
