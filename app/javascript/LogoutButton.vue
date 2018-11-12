b8a65f6b3387da6c3c43dbba2dfd1d10a080007d
Agustin ORTU <ortu.agustin@gmail.com>
2018-11-12 14:01:03 -0300

    Usabilidad - Logout: - Eliminada confirmacion - Cambiado boton, ahora es solo icono con tooltip y se elimino el texto

--- app/javascript/LogoutButton.vue
+++ app/javascript/LogoutButton.vue
@@ -1,28 +1,18 @@
 <template>
-  <a href="#!" @click.prevent="clicked">
-    <slot></slot>
+  <a href="#!" @click.prevent="clicked" title="Cerrar Sesión">
+    <i class="fas fa-sign-out-alt fa-lg"></i>
   </a>
 </template>
 
 <script>
 export default {
-  props: ['url', 'title', 'text', 'type', 'confirm-button', 'cancel-button'],
+  props: [ 'logoutUrl', 'redirectUrl' ],
 
   methods: {
     clicked() {
-      swal({
-        title: this.title,
-        type: this.type,
-        showCancelButton: true,
-        // confirmButtonColor: "#3085d6",
-        // cancelButtonColor: "#d33",
-        confirmButtonText: this.confirmButton,
-        cancelButtonText: this.cancelButton,
-      }).then(result => {
-        if (result.value) {
-          axios.get(this.url).then((data) => location.href = 'http://localhost:3000')
-        }
-      });
+      axios
+        .get(this.logoutUrl)
+        .then(() => location.href = this.redirectUrl)
     }
   }
 };
