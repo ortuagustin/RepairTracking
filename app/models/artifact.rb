9f5c1464967ab015d0dd20a998eb174add17ece1
Agustin ORTU <ortu.agustin@gmail.com>
2018-11-21 17:27:37 -0300

    relacion entre artefactos y piezas

--- app/models/artifact.rb
+++ app/models/artifact.rb
@@ -1,5 +1,7 @@
 class Artifact < ApplicationRecord
   has_many :repairs, dependent: :delete_all
+  has_many :pieces, dependent: :delete_all
+
   validates :name, :model, presence: true
   validates :model, uniqueness: true
 end
